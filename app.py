4af4051a2c045ab405a2609b26e0886e800974e3
Berke Dilekoglu <berkedilekoglu@gmail.com>
2023-08-27 16:39:53 +0300

    main function added for prod

--- app.py
+++ app.py
@@ -4,19 +4,25 @@ from streamlit_option_menu import option_menu
 from prediction_page import show_predict_page
 from about_page import show_about_content
 from tutorial_page import show_tutorial_page
-st.set_page_config(layout="wide")
-# 2. horizontal menu
-page = option_menu('SUMOnet', ["About", "Predictions", "Tutorial"], 
-    icons=['house', 'gear','bi-book'], 
-    menu_icon="cast", default_index=0, orientation="horizontal")
-if page == "About":
-    show_about_content()
-    
-elif page == "Predictions":
-    show_predict_page()
 
-elif page == "Tutorial":
-    show_tutorial_page()
+
+def main():
+    # 2. horizontal menu
+    page = option_menu('SUMOnet', ["About", "Predictions", "Tutorial"], 
+        icons=['house', 'gear','bi-book'], 
+        menu_icon="cast", default_index=0, orientation="horizontal")
+    if page == "About":
+        show_about_content()
+        
+    elif page == "Predictions":
+        show_predict_page()
+
+    elif page == "Tutorial":
+        show_tutorial_page()
+
+if __name__ == "__main__":
+    st.set_page_config(layout="wide")
+    main()
     
 
 
